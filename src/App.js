66d94fd6f3a357980c205456ba5cb218d9cd400b
rodrigo barbosa <rodrigodot42@gmail.com>
2023-11-18 11:51:03 -0300

    chore: update docker config

--- src/App.js
+++ src/App.js
@@ -3,29 +3,42 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-   	const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
 
-	useEffect(() => {
-		fetch("$BACKEND_URL")
-		 .then((res) => res.json())
-      		 .then((result) => setData(result))
-      		 .catch((err) => console.log("error"));
-	}, []);
+  useEffect(() => {
+    fetch("$BACKEND_URL")
+      .then((res) => res.json())
+      .then((result) => setData(result))
+      .catch((err) => console.log("error"));
+
+
+      setData([
+        {
+          title: 'TILE',
+          artis: 'ART',
+          genre: 'ROCK',
+          song_id: '42'
+        }
+      ])
+  }, []);
+
   return (
-    <div class="divTable">
+    <div className="divTable">
       {data &&
-        data.map((element, index) => (
-        <div class="headRow">
-          <div class="divCell" align="center">{element.song_id}</div>
-          <div class="divCell divCellName">{element.title}</div>
-          <div class="divCell divCellName">{element.artist}</div>
-          <div class="divCell" align="center">{element.genre}</div>
+        data.map((element) => (
+          <div class="headRow">
+            <div class="divCell" align="center">
+              {element.song_id}
+            </div>
+            <div class="divCell divCellName">{element.title}</div>
+            <div class="divCell divCellName">{element.artist}</div>
+            <div class="divCell" align="center">
+              {element.genre}
+            </div>
           </div>
         ))}
     </div>
   );
-
 }
 
 export default App;
-
